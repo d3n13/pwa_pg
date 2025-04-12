548568d1bfbf5aa0dca13824eef6a852450561bb
d3n13 <46310638+d3n13@users.noreply.github.com>
2025-04-12 11:10:01 +0000

    Deploying to gh-pages from @ d3n13/pwa_pg@e91518ddabcdf5baa1ccd46b20e524def1134e52 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pwa_pg/sw.js', { scope: '/pwa_pg/' })})}
