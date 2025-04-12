e934d796d376e2b5694e1671d0a1fdb2a72a81dd
Denis Bilenko <goldenrodcap@gmail.com>
2025-04-12 11:19:25 +0400

    fix: basepath

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <HashRouter basename="/d3n13/pwa_pg">
+    <HashRouter basename="/pwa_pg">
       <Routes>
         <Route
           path="/foo"
