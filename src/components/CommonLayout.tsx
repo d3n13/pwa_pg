d0c7dd9e3365e40396ea0f60dac73cbe0b050980
Denis Bilenko <goldenrodcap@gmail.com>
2025-04-12 15:00:44 +0400

    feat: add link to a page that don't exist

--- src/components/CommonLayout.tsx
+++ src/components/CommonLayout.tsx
@@ -8,6 +8,9 @@ export function CommonLayout() {
         <Link to={"/foo"}>Foo page</Link>
         <Link to={"/bar"}>Bar page</Link>
         <Link to={"/bar/123"}>Bar 123 page</Link>
+        <Link to={"/this-page-dont-exist"}>
+          Link to a page that don't exist
+        </Link>
       </nav>
       <Outlet />
     </>
